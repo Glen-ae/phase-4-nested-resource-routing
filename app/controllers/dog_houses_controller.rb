4880cb3dfddcd5879d7919cc436d429934591a43
Glen <glen.kimani@student.moringaschool.com>
2023-03-20 23:21:23 +0300

    Completed

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -13,3 +13,15 @@ class DogHousesController < ApplicationController
   end
 
 end
+
+
+def reviews_index
+  dog_house = DogHouse.find(params[:dog_house_id])
+  reviews = dog_house.reviews
+  render json: reviews, include: :dog_house
+end
+
+def review
+  review = Review.find(params[:id])
+  render json: review, include: :dog_house
+end
